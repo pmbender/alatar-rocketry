9918de8d6389790a9dbd728c6c8639cd5e28e5b5
alataristarion <alataristarion@gmail.com>
2025-01-03 14:05:56 -0600

    Updated to break data into sub-files based on prototype.
    +entity.lua
    +item.lua
    +recipe.lua

---
+++ prototypes/item.lua
@@ -0,0 +1,17 @@
+local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
+
+data:extend({
+    {
+        type = "item",
+        name = "vac-thruster",
+        icon = "__realistic-rocketry__/graphics/icons/vac-thruster.png",
+        subgroup = "space-platform",
+        order = "f[vac-thruster]",
+        inventory_move_sound = space_age_item_sounds.rocket_inventory_move,
+        pick_sound = space_age_item_sounds.rocket_inventory_pickup,
+        drop_sound = space_age_item_sounds.rocket_inventory_move,
+--        place_result = "vac-thruster",
+        stack_size = 10,
+        weight = 300*kg
+    },
+})
---
+++ prototypes/recipe.lua
@@ -0,0 +1,15 @@
+data:extend({
+    {
+    type = "recipe",
+    name = "vac-thruster",
+    enabled = true,
+    ingredients =
+    {
+      {type = "item", name = "steel-plate", amount = 10},
+      {type = "item", name = "processing-unit", amount = 10},
+      {type = "item", name = "electric-engine-unit", amount = 5}
+    },
+    energy_required = 10,
+    results = {{type="item", name="vac-thruster", amount=1}}
+  }
+})
